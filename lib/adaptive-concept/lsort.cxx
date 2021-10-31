0f4eb2cf278fa233ff056fcf04a58e45515a8aab
Diego Rossinelli <rossinelli.diego@gmail.com>
2021-10-31 21:16:32 +0100

    still working on values

--- lib/adaptive-concept/lsort.cxx
+++ lib/adaptive-concept/lsort.cxx
@@ -1,10 +1,38 @@
+#include <cstdio>
 #include <cstddef>
 #include <cstdint>
+#include <cstring>
 #include <cassert>
 
 #include <limits>
 #include <algorithm>
 #include <utility>
+
+template < typename C, typename I, typename V >
+static void gatherciv (
+		const C count,
+		const V * __restrict__ const in,
+		const I * __restrict__ const idx,
+		V * __restrict__ const out )
+    {
+		for (C i = 0; i < count; ++i)
+				out[i] = in[idx[i]];
+    }
+
+template < typename C, typename I >
+static void gather (
+	const size_t vsz,
+	const C count,
+	const void * __restrict__ const in,
+	const I * __restrict__ const idx,
+	void * __restrict__ const out )
+    {
+		switch (vsz)
+		{
+		case 1 :
+			gatherciv(count, in
+		}
+    }
 
 template < typename T >
 static void sortk (
@@ -14,38 +42,81 @@ static void sortk (
 	std::sort(k, k + c);
 }
 
+template < typename K, typename C >
+static void sortkcv_indirect (
+	const size_t vsz,
+	const C c,
+	K * const k,
+	void * const v )
+{
+	typedef std::pair<K, C> KI_t;
+
+	KI_t * t = (KI_t *)malloc(sizeof(*t) * c);
+
+	for (C i = 0; i < c; ++i)
+		t[i] = (KI_t){ .first = k[i], .second = i };
+
+	std::sort(t, t + c);
+
+	void * v2 = malloc(vsz * c);
+	memcpy(v2, v, vsz * c);
+
+	enum { BUNCH = 1 << 12 };
+
+	for (ptrdiff_t base = 0; base < c; base += BUNCH)
+	{
+		const C n = (C)std::min((ptrdiff_t)c - base, (ptrdiff_t)BUNCH);
+
+		const KI_t * iki = t + base;
+		
+		K * ok = k + base;
+		for (C i = 0; i < n; ++i)
+			ok[i] = iki[i].first;
+
+		C ord[BUNCH];
+		for (C i = 0; i < n; ++i)
+			ord[i] = iki[i].second;
+
+		gather(vsz, n, v2, ord, v + base);
+	}
+
+	free(t);
+	free(v2);
+}
+
 template < typename K, typename C, typename V >
 static void sortkcv_direct (
 	const C c,
 	K * k,
 	V * v )
 {
-		typedef std::pair<K, V> KV_t;
-		
-		KV_t * t = new KV_t[c];
-		for (C i = 0; i < c; ++i)
-			t[i] = (KV_t){ .first = k[i], .second = v[i] };
+	typedef std::pair<K, V> KV_t;
 
-		std::sort(t, t + c); 
+	KV_t * t = (KV_t *)malloc(sizeof(*t) * c);
 
-		enum { BUNCH = 1 << 12 };
+	for (C i = 0; i < c; ++i)
+		t[i] = (KV_t){ .first = k[i], .second = v[i] };
 
-		for (ptrdiff_t base = 0; base < c; base += BUNCH)
-		{
-			const C n = (C)std::min(c - base, (ptrdiff_t)BUNCH);
-			
-			KV_t * ikv = t + base;
+	std::sort(t, t + c);
 
-			K * ok = k + base;
-			for (C i = 0; i < c; ++i)
-				ok[i] = ikv[i].first;
+	enum { BUNCH = 1 << 12 };
 
-			V * ov = v + base;
-			for (C i = 0; i < c; ++i)
-				ov[i] = ikv[i].first;
+	for (ptrdiff_t base = 0; base < c; base += BUNCH)
+	{
+		const C n = (C)std::min((ptrdiff_t)c - base, (ptrdiff_t)BUNCH);
 
-				delete [] t;
-		}
+		KV_t * ikv = t + base;
+
+		K * ok = k + base;
+		for (C i = 0; i < n; ++i)
+			ok[i] = ikv[i].first;
+
+		V * ov = v + base;
+		for (C i = 0; i < n; ++i)
+			ov[i] = ikv[i].second;
+	}
+
+	free(t);
 }
 
 template < typename K, typename C >
@@ -56,19 +127,28 @@ static void sortkcv (
 	void * v )
 {
 	if (vsz <= sizeof(c))
-	{
 		switch (vsz)
 		{
 		case 1 :
-			sortkvc_direct(c, k, (uint8_t *)v);
+			sortkcv_direct(c, k, (uint8_t *)v);
+			break;
 		case 2 :
-			sortkvc_direct(c, k, (uint8_t *)v);
+			sortkcv_direct(c, k, (uint16_t *)v);
+			break;
 		case 4 :
-			sortkvc_direct(c, k, (uint8_t *)v);
-	}
+			sortkcv_direct(c, k, (uint32_t *)v);
+			break;
+		case 8 :
+			sortkcv_direct(c, k, (uint64_t *)v);
+			break;
+		default:
+			sortkcv_indirect(vsz, c, k, v);
+		}
+	else
+		sortkcv_indirect(vsz, c, k, v);
 }
 
-template < typename K, typename V >
+template < typename K >
 static void sortkc (
 	const ptrdiff_t vsz,
 	const ptrdiff_t c,
@@ -86,14 +166,14 @@ void lsortu(
 	const ptrdiff_t ksz,
 	const ptrdiff_t vsz,
 	const ptrdiff_t cnt,
-	void * k, 
+	void * k,
 	void * v)
 {
 	switch (ksz)
 	{
 	case 8 :
 		if (v)
-			sortkc(cnt, vsz, (uint64_t *)k, v);
+			sortkc(vsz, cnt, (uint64_t *)k, v);
 		else
 			sortk(cnt, (uint64_t *)k);
 
@@ -101,7 +181,7 @@ void lsortu(
 
 	case 4 :
 		if (v)
-			sortkc(cnt, vsz, (uint32_t *)k, v);
+			sortkc(vsz, cnt, (uint32_t *)k, v);
 		else
 			sortk(cnt, (uint32_t *)k);
 
@@ -109,7 +189,7 @@ void lsortu(
 
 	case 2 :
 		if (v)
-			sortkc(cnt, vsz, (uint16_t *)k, v);
+			sortkc(vsz, cnt, (uint16_t *)k, v);
 		else
 			sortk(cnt, (uint16_t *)k);
 
@@ -117,7 +197,7 @@ void lsortu(
 
 	case 1 :
 		if (v)
-			sortkc(cnt, vsz, (uint8_t *)k, v);
+			sortkc(vsz, cnt, (uint8_t *)k, v);
 		else
 			sortk(cnt, (uint16_t *)k);
 
