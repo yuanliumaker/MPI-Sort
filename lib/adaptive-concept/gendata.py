c365efec5c97482bcd28205b5863fc690ff9754a
Diego Rossinelli <rossinelli.diego@gmail.com>
2021-11-01 21:04:31 +0100

    sparse sorting

--- lib/adaptive-concept/gendata.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-np.random.randint(2, size=10).astype(np.uint32).tofile('ciccio.raw')
-np.arange(10, dtype = np.uint32).tofile('ciccia.raw')
-(313 * np.ones(7, dtype = np.uint32)).tofile('cicche.raw')
-
-np.array([3, 1, 1, 0, 0, 5, 4, 2, 0, 0], dtype=np.uint32).tofile('example.raw')
